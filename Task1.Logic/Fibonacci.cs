a1b792a8347b3eb8ec1e74f7f6d691c83490b397
hankovich <svyatoslav.hankovich@gmail.com>
2016-11-17 12:30:51 +0300

    Init commit. Task1 (Fibonacci) added.

---
+++ Task1.Logic/Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Logic
+{
+    public class Fibonacci
+    {
+        /// <summary>
+        /// Get Fibonacci sequance
+        /// </summary>
+        /// <returns>Fibonacci sequance</returns>
+        public static IEnumerable<long> GetSequence()
+        {
+            long prelast = 1;
+            long last = 0;
+            long current;
+            while (true)
+            {
+                yield return last;
+                try
+                {
+                    checked
+                    {
+                        current = last + prelast;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    yield break;
+                }
+                prelast = last;
+                last = current;
+            }
+        }
+    }
+}
---
+++ Task1.Tests/FibonacciTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1.Logic;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(50, 12586269025)]
+        [Test]
+        public void GetSequence(int number, long expected)
+        {
+            IEnumerator<long> sequence = Fibonacci.GetSequence().GetEnumerator();
+            bool b = false;
+            for (int i = 0; i <= number; i++)
+                b = sequence.MoveNext();
+            if (!b)
+                Assert.Fail($"Smth gonna wrong at number {number}");
+            Assert.AreEqual(expected, sequence.Current);
+        }
+    }
+}
